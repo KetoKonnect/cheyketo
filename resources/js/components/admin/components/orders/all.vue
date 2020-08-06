894ea68bc1b6eb5bcfd19cae43cfa8dec15fcbb6
ejay010 <ejay010@gmail.com>
2020-08-05 20:14:59 -0400

    Molly

--- resources/js/components/admin/components/orders/all.vue
+++ resources/js/components/admin/components/orders/all.vue
@@ -1,76 +1,69 @@
 <template>
-    <div class="container">
-        <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item">
-                    <router-link :to="{ name: 'admin_home' }">Home</router-link>
-                </li>
-                <li class="breadcrumb-item active" aria-current="page">
-                    All Orders
-                </li>
-            </ol>
-        </nav>
+  <div class="container">
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+          <router-link :to="{ name: 'admin_home' }">Home</router-link>
+        </li>
+        <li class="breadcrumb-item active" aria-current="page">All Orders</li>
+      </ol>
+    </nav>
 
-        <h2>All Orders</h2>
+    <h2>All Orders</h2>
 
-        <div class="row">
-            <div class="col">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table class="table">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">ID</th>
-                                        <th scope="col">Customer</th>
-                                        <th scope="col">Total</th>
-                                        <th scope="col">Status</th>
-                                        <th scope="col">Date Placed</th>
-                                        <th scope="col">Action</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr v-for="order in orders" :key="order.id">
-                                        <td>{{ order.id }}</td>
-                                        <td>{{ order.customer.name }}</td>
-                                        <td>${{ order.total }}</td>
-                                        <td>{{ order.status }}</td>
-                                        <td>{{ order.created_at }}</td>
-                                        <td>
-                                            <a
-                                                :href="view_url(order.id)"
-                                                class="btn btn-outline-secondary"
-                                            >
-                                                view
-                                            </a>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
+    <div class="row">
+      <div class="col">
+        <div class="card">
+          <div class="card-body">
+            <div class="table-responsive">
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Customer</th>
+                    <th scope="col">Total</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Date Placed</th>
+                    <th scope="col">Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="order in orders" :key="order.id">
+                    <td>{{ order.id }}</td>
+                    <td>{{ order.customer.name }}</td>
+                    <td>${{ order.total }}</td>
+                    <td>{{ order.status }}</td>
+                    <td>{{ order.created_at }}</td>
+                    <td>
+                      <a :href="view_url(order.id)" class="btn btn-outline-secondary">view</a>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    mounted() {
-        axios.get(process.env.MIX_ADMIN_URL + "/api/orders").then(response => {
-            this.orders = response.data.data;
-        });
+  mounted() {
+    axios.get("/admin/api/orders").then((response) => {
+      this.orders = response.data.data;
+    });
+  },
+  data() {
+    return {
+      orders: [],
+    };
+  },
+  methods: {
+    view_url(order_id) {
+      return "/admin/orders/" + order_id;
     },
-    data() {
-        return {
-            orders: []
-        };
-    },
-    methods: {
-        view_url(order_id) {
-            return process.env.MIX_ADMIN_URL + "/orders/" + order_id;
-        }
-    }
+  },
 };
 </script>
--- resources/js/components/admin/components/products/all.vue
+++ resources/js/components/admin/components/products/all.vue
@@ -1,137 +1,115 @@
 <template>
-    <div class="container">
-        <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item">
-                    <router-link :to="{ name: 'admin_home' }">Home</router-link>
-                </li>
-                <li class="breadcrumb-item active" aria-current="page">
-                    All Products
-                </li>
-            </ol>
-        </nav>
+  <div class="container">
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+          <router-link :to="{ name: 'admin_home' }">Home</router-link>
+        </li>
+        <li class="breadcrumb-item active" aria-current="page">All Products</li>
+      </ol>
+    </nav>
 
-        <div class="row">
-            <div class="col">
-                <div class="card">
-                    <div class="card-body">
-                        <h2>All Products</h2>
-                        <a
-                            :href="add_products_url"
-                            class="btn btn-outline-secondary"
-                            >Add a Product</a
-                        >
-                        <div class="table-responsive mt-3">
-                            <table class="table">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">ID</th>
-                                        <th scope="col">Name</th>
-                                        <th scope="col">Description</th>
-                                        <th scope="col">Qty Avail.</th>
-                                        <th scope="col">Date created</th>
-                                        <th scope="col">Action</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr
-                                        v-for="(product, index) in products"
-                                        :key="product.id"
-                                    >
-                                        <td>{{ product.id }}</td>
-                                        <td>{{ product.name }}</td>
-                                        <td>{{ product.description }}</td>
-                                        <td>{{ product.qty }}</td>
-                                        <td>
-                                            {{ product.created_at }}
-                                        </td>
-                                        <td>
-                                            <router-link
-                                                :to="{
+    <div class="row">
+      <div class="col">
+        <div class="card">
+          <div class="card-body">
+            <h2>All Products</h2>
+            <a :href="add_products_url" class="btn btn-outline-secondary">Add a Product</a>
+            <div class="table-responsive mt-3">
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Qty Avail.</th>
+                    <th scope="col">Date created</th>
+                    <th scope="col">Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="(product, index) in products" :key="product.id">
+                    <td>{{ product.id }}</td>
+                    <td>{{ product.name }}</td>
+                    <td>{{ product.description }}</td>
+                    <td>{{ product.qty }}</td>
+                    <td>{{ product.created_at }}</td>
+                    <td>
+                      <router-link
+                        :to="{
                                                     name: 'view_product',
                                                     params: { id: product.id }
                                                 }"
-                                                :disabled="disabled"
-                                                class="btn btn-outline-secondary"
-                                            >
-                                                view
-                                            </router-link>
-                                            <button
-                                                @click="
+                        :disabled="disabled"
+                        class="btn btn-outline-secondary"
+                      >view</router-link>
+                      <button
+                        @click="
                                                     delete_this(
                                                         product.id,
                                                         index
                                                     )
                                                 "
-                                                class="btn btn-outline-danger"
-                                                :disabled="disabled"
-                                            >
-                                                Delete
-                                            </button>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
+                        class="btn btn-outline-danger"
+                        :disabled="disabled"
+                      >Delete</button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
 export default {
-    mounted() {
-        axios
-            .get(process.env.MIX_ADMIN_URL + "/api/products")
-            .then(response => {
-                this.products = response.data.data;
-            });
+  mounted() {
+    axios.get("/admin/api/products").then((response) => {
+      this.products = response.data.data;
+    });
+  },
+  data() {
+    return {
+      products: [],
+      disabled: false,
+    };
+  },
+  computed: {
+    add_products_url() {
+      return "/admin/products/create";
     },
-    data() {
-        return {
-            products: [],
-            disabled: false
-        };
+  },
+  methods: {
+    view_url(product_id) {
+      return "/admin/products/" + product_id;
     },
-    computed: {
-        add_products_url() {
-            return process.env.MIX_ADMIN_URL + "/products/create";
-        }
-    },
-    methods: {
-        view_url(product_id) {
-            return process.env.MIX_ADMIN_URL + "/products/" + product_id;
-        },
-        delete_this(product_id, index) {
-            this.disabled = true;
-            Swal.fire({
-                title: "Delete?",
-                text: "Please confirm Delete",
-                icon: "warning",
-                showCancelButton: true,
-                confirmButtonText: "Yes, Delete this"
-            }).then(result => {
-                if (result.value) {
-                    axios
-                        .get(
-                            process.env.MIX_ADMIN_URL +
-                                "/api/products/" +
-                                product_id +
-                                "/delete"
-                        )
-                        .then(response => {
-                            this.products.splice(index, 1);
-                            this.disabled = false;
-                            Swal.fire({
-                                title: "Product Deleted",
-                                text: "Product completely deleted",
-                                icon: "success"
-                            });
-                        });
-                }
+    delete_this(product_id, index) {
+      this.disabled = true;
+      Swal.fire({
+        title: "Delete?",
+        text: "Please confirm Delete",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Yes, Delete this",
+      }).then((result) => {
+        if (result.value) {
+          axios
+            .get("/admin/api/products/" + product_id + "/delete")
+            .then((response) => {
+              this.products.splice(index, 1);
+              this.disabled = false;
+              Swal.fire({
+                title: "Product Deleted",
+                text: "Product completely deleted",
+                icon: "success",
+              });
             });
         }
-    }
+      });
+    },
+  },
 };
 </script>
